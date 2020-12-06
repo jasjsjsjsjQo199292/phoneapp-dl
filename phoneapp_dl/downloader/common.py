5e805fd98b758d984b5f7090f5bfe4ef1a4541cc
jasjsjsjsjQo199292 <74087082+jasjsjsjsjQo199292@users.noreply.github.com>
2020-12-06 03:09:39 -0800

    Update common.py

--- phoneapp_dl/downloader/common.py
+++ phoneapp_dl/downloader/common.py
@@ -1,1 +1,26 @@
- 
+ import os
+from .. import config, util
+
+
+class DownloaderBase():
+    """Base class for downloaders"""
+    scheme = ""
+
+    def __init__(self, job):
+        self.out = job.out
+        self.session = job.extractor.session
+        self.part = self.config("part", True)
+        self.partdir = self.config("part-directory")
+        self.log = job.get_logger("downloader." + self.scheme)
+
+        if self.partdir:
+            self.partdir = util.expand_path(self.partdir)
+            os.makedirs(self.partdir, exist_ok=True)
+
+    def config(self, key, default=None):
+        """Interpolate downloader config value for 'key'"""
+        return config.interpolate(("downloader", self.scheme), key, default)
+
+    def download(self, url, pathfmt):
+        """Write data from 'url' into the file specified by 'pathfmt'"""
+
