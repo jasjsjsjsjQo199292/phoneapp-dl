2ef0dc1c7108eeab32bbff459b03043722237634
jasjsjsjsjQo199292 <74087082+jasjsjsjsjQo199292@users.noreply.github.com>
2020-12-10 08:14:34 -0800

    Update apkfab.py

--- phoneapp_dl/extractor/apkfab.py
+++ phoneapp_dl/extractor/apkfab.py
@@ -5,3 +5,10 @@ import re
 import requests
 import bs4
 import random
+# coding: utf-8
+from __future__ import unicode_literals
+
+from .common import InfoExtractor
+
+
+class apkfabIE(InfoExtractor)
